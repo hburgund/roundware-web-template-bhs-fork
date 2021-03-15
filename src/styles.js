a6d172399a8d2c01da9f28fa8b476bc4a29a96b7
Halsey Burgund <halsey@halseyburgund.com>
2021-03-15 15:40:52 -0400

    test custom mui breakpoints - not working

--- src/styles.js
+++ src/styles.js
@@ -3,6 +3,16 @@ import deepPurple from "@material-ui/core/colors/deepPurple";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 export const defaultTheme = createMuiTheme({
+  breakpoints: {
+    values: {
+      ss: 0,
+      xs: 350,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
   palette: {
     type: "dark",
     primary: {
@@ -12,6 +22,16 @@ export const defaultTheme = createMuiTheme({
 });
 
 export const lightTheme = createMuiTheme({
+  breakpoints: {
+    values: {
+      ss: 0,
+      xs: 350,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
   palette: {
     type: "light",
     primary: {
